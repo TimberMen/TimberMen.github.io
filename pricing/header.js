10e5d7865a7da5bdce1b0c418a422907bc7db4a7
Sam <merante.s@hotmail.com>
2018-03-19 17:53:56 -0400

    Updated price directory

---
+++ pricing/header.js
@@ -0,0 +1,8 @@
+function init(){
+	var html =  `<ul>
+					<li><a href="../index.html">Home</a></li>
+					<li><a href="./pricing.html">Pricing</a></li>
+				</ul>`;
+	document.getElementById("header").innerHTML = html;
+}
+init();
