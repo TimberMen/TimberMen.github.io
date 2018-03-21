7ae43e9bbdfec464c9204bb91bd7139462ff1a72
Sam M <merante.s@hotmail.com>
2018-03-21 12:14:53 -0400

    Updated pricing page

--- pricing/main.js
+++ pricing/main.js
@@ -1,14 +1,17 @@
 var slider = document.getElementById("deweeding-slider");
  var deweedFt = document.getElementById("deweeding-ft");
+var deweedLight = document.getElementById("Light");
+var deweedMedium = document.getElementById("Medium");
+var deweedHeavy = document.getElementById("Heavy");
  var deweedPrice;
  var deweedAmountPerFoot;
 
 function init(){
   deweedPrice = document.getElementById("deweeding-price");
-
-  deweedAmountPerFoot = 10;
+  deweedAmountPerFoot = 20;
   deweedFt.value = slider.value;
   deweedPrice.innerHTML = slider.value*deweedAmountPerFoot;
+  deweedMedium.checked=true;
 }
 
 slider.oninput = function() {
@@ -20,5 +23,24 @@ deweedFt.oninput = function(){
   deweedPrice.innerHTML = this.value*deweedAmountPerFoot;
   slider.value=this.value;
 }
+deweedLight.onclick = function(){
+  deweedAmountPerFoot = 15;
+  deweedPrice.innerHTML = slider.value*deweedAmountPerFoot;
+  deweedMedium.checked=false;
+  deweedHeavy.checked=false;
+}
 
+deweedMedium.onclick = function(){
+ deweedAmountPerFoot = 20;
+ deweedPrice.innerHTML = slider.value*deweedAmountPerFoot;
+  deweedLight.checked=false;
+  deweedHeavy.checked=false;
+}
+
+deweedHeavy.onclick = function(){
+  deweedAmountPerFoot = 30;
+  deweedPrice.innerHTML = slider.value*deweedAmountPerFoot;
+  deweedMedium.checked=false;
+  deweedLight.checked=false;
+}
 init();
