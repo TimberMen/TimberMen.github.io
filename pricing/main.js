0af7d31a31d96d9d735a59713cb9abf8d5c5363f
Sam M <merante.s@hotmail.com>
2018-03-21 15:38:20 -0400

    pricing update for mowing

--- pricing/main.js
+++ pricing/main.js
@@ -1,46 +1,109 @@
 var slider = document.getElementById("deweeding-slider");
- var deweedFt = document.getElementById("deweeding-ft");
+var deweedFt = document.getElementById("deweeding-ft");
 var deweedLight = document.getElementById("Light");
 var deweedMedium = document.getElementById("Medium");
 var deweedHeavy = document.getElementById("Heavy");
- var deweedPrice;
- var deweedAmountPerFoot;
+var deweedPrice;
+var deweedAmountPerFoot;
 
+var mowingSlider = document.getElementById("mowing-slider");
+var mowingFt = document.getElementById("mowing-ft");
+var Light1 = document.getElementById("Light1");
+var Medium1 = document.getElementById("Medium1");
+var Heavy1 = document.getElementById("Heavy1");
+
+var mowingPrice;
+var mowingAmountPerFoot;
+
+var totalPrice;
 function init(){
   deweedPrice = document.getElementById("deweeding-price");
-  deweedAmountPerFoot = 15;
+  deweedAmountPerFoot = 5;
   deweedFt.value = slider.value;
   deweedPrice.innerHTML = slider.value*deweedAmountPerFoot;
   deweedLight.checked=true;
-}
 
+
+  mowingPrice = document.getElementById("mowing-price");
+  mowingAmountPerFoot = 0.05;
+  mowingFt.value = mowingSlider.value;
+  mowingPrice.innerHTML = mowingSlider.value*mowingAmountPerFoot;
+  Medium1.checked=true;
+
+  totalPrice = document.getElementById("total-price");
+}
+init();
+calcTotal();
+
+function calcTotal(){
+  totalPrice.innerHTML = parseFloat(deweedPrice.innerHTML) + parseFloat(mowingPrice.innerHTML);
+}
 slider.oninput = function() {
   deweedPrice.innerHTML = this.value*deweedAmountPerFoot;
-  deweedFt.value=this.value;
+  deweedFt.value=this.value
+  calcTotal();
 }
 
 deweedFt.oninput = function(){
   deweedPrice.innerHTML = this.value*deweedAmountPerFoot;
   slider.value=this.value;
+  calcTotal();
 }
 deweedLight.onclick = function(){
-  deweedAmountPerFoot = 15;
+  deweedAmountPerFoot = 5;
   deweedPrice.innerHTML = slider.value*deweedAmountPerFoot;
   deweedMedium.checked=false;
   deweedHeavy.checked=false;
+  calcTotal();
 }
 
 deweedMedium.onclick = function(){
- deweedAmountPerFoot = 20;
+ deweedAmountPerFoot = 10;
  deweedPrice.innerHTML = slider.value*deweedAmountPerFoot;
   deweedLight.checked=false;
   deweedHeavy.checked=false;
+  calcTotal();
 }
 
 deweedHeavy.onclick = function(){
-  deweedAmountPerFoot = 30;
+  deweedAmountPerFoot = 15;
   deweedPrice.innerHTML = slider.value*deweedAmountPerFoot;
   deweedMedium.checked=false;
   deweedLight.checked=false;
+  calcTotal();
 }
-init();
+/******************************************************************************/
+mowingSlider.oninput = function() {
+  mowingPrice.innerHTML = this.value*mowingAmountPerFoot;
+  mowingFt.value=this.value;
+  calcTotal();
+}
+
+mowingFt.oninput = function(){
+  mowingPrice.innerHTML = this.value*mowingAmountPerFoot;
+  mowingSlider.value=this.value;
+  calcTotal();
+}
+Light1.onclick = function(){
+  mowingAmountPerFoot = 0.025;
+  mowingPrice.innerHTML = mowingSlider.value*mowingAmountPerFoot;
+  Medium1.checked=false;
+  Heavy1.checked=false;
+  calcTotal();
+}
+
+Medium1.onclick = function(){
+  mowingAmountPerFoot = .05;
+  mowingPrice.innerHTML = mowingSlider.value*mowingAmountPerFoot;
+  Light1.checked=false;
+  Heavy1.checked=false;
+  calcTotal();
+}
+
+Heavy1.onclick = function(){
+  mowingAmountPerFoot = .1;
+  mowingPrice.innerHTML = mowingSlider.value*mowingAmountPerFoot;
+  Medium1.checked=false;
+  Light1.checked=false;
+  calcTotal();
+}
