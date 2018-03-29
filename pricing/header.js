63a9d100b3435e1369d113156a8646ecdcfa2ea4
Sam <merante.s@hotmail.com>
2018-03-29 13:09:18 -0400

    updated templates for ie11

--- pricing/header.js
+++ pricing/header.js
@@ -1,19 +1,17 @@
 function init(){
-	var html =
-`
-	<h1 class="title"> Timber Men  Vaughan</h1>
-	<div class ="nav-bar">
-	<div class="container">
-		<img class="bg-image" src="../pics/smallBg.png"></img>
-	</div>
-		<ul>
-		<li><a href="../index.html">Home</a></li>
-		<li><a href="../projects/projects.html">Projects</a></li>
-		<li><a href="./pricing.html" class="active">Pricing</a></li>
-		</ul>
-	<div>
-	<hr/>
-`;
+	var html =""+
+	'<h1 class="title"> Timber Men  Vaughan</h1>'+
+	'<div class ="nav-bar">'+
+	'<div class="container">'+
+		'<img class="bg-image" src="../pics/smallBg.png"></img>'+
+	'</div>'+
+	'	<ul>'+
+	'	<li><a href="../index.html">Home</a></li>'+
+	'	<li><a href="../projects/projects.html">Projects</a></li>'+
+	'	<li><a href="./pricing.html" class="active">Pricing</a></li>'+
+	'	</ul>'+
+	'<div>'+
+	'<hr/>';
 	document.getElementById("header").innerHTML = html;
 }
 
